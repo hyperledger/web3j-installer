a9861952df765fa119dba2da82dfe1e03d8f9025
Nischal Sharma <nischal@web3labs.com>
2024-10-08 17:36:20 +0530

    fix in memory using curl script content
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-# URL to the checksum file
+# URL to the checksum file on GitHub
 CHECKSUM_URL="https://raw.githubusercontent.com/hyperledger/web3j-installer/main/checksum-linux.txt"
 
 # Function to fetch the pre-calculated checksum from GitHub
@@ -8,21 +7,43 @@ fetch_checksum() {
     curl --silent "$CHECKSUM_URL"
 }
 
-# Function to calculate checksum from script content (works for in-memory or file-based)
-calculate_checksum() {
-    script_content="$1"
+# Function to calculate the checksum of the in-memory script content
+calculate_in_memory_checksum() {
+    script_content=$(curl --silent -L get.web3j.io)
+    filtered_content=$(echo "$script_content" | sed '/^CHECKSUM_URL=/d')
     if [[ "$(uname)" == "Darwin" ]]; then
-      echo "$script_content" | sed '/^CHECKSUM_URL=/d' | shasum -a 256 | awk '{print $1}'
+      echo "$filtered_content" | shasum -a 256 | awk '{print $1}'
     else
-      echo "$script_content" | sed '/^CHECKSUM_URL=/d' | sha256sum | awk '{print $1}'
+      echo "$filtered_content" | sha256sum | awk '{print $1}'
     fi
+}
+
+# Function to calculate the checksum of the script file (when not running in memory)
+calculate_file_checksum() {
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS: use `shasum`
+    sed '/^CHECKSUM_URL=/d' "$0" | shasum -a 256 | awk '{print $1}'
+  else
+    # Linux: use `sha256sum`
+    sed '/^CHECKSUM_URL=/d' "$0" | sha256sum | awk '{print $1}'
+  fi
+}
+
+is_piped_execution() {
+  [ -p /dev/stdin ]
 }
 
 # Verify the integrity of the script
 verify_checksum() {
-  script_content="$1"
   FETCHED_CHECKSUM=$(fetch_checksum)
-  CURRENT_CHECKSUM=$(calculate_checksum "$script_content")
+
+  if is_piped_execution; then
+    echo "Running in-memory execution"
+    CURRENT_CHECKSUM=$(calculate_in_memory_checksum)
+  else
+    echo "Running from a file"
+    CURRENT_CHECKSUM=$(calculate_file_checksum)
+  fi
 
   if [ "$CURRENT_CHECKSUM" = "$FETCHED_CHECKSUM" ]; then
     echo "Checksum verification passed!"
@@ -32,34 +53,18 @@ verify_checksum() {
   fi
 }
 
-main() {
-  # Check if the script is being piped (i.e., in-memory execution)
-  cat
-  if [ -p /dev/stdin ]; then
-    script_content=$(cat /dev/fd/0)
-    verify_checksum "$script_content"
+# Run checksum verification
+verify_checksum
 
-    # Execute the script content
-    echo "$script_content" | sh
-  else
-    script_content=$(cat "$0")
-    verify_checksum "$script_content"
-    # Proceed to the actual logic after checksum verification
-    setup_color
-    check_java_version
-    check_if_installed
-    if [ $installed_flag -eq 1 ]; then
-      check_if_web3j_homebrew
-      check_version
-      clean_up
-      install_web3j
-      source_web3j
-      completed
-    else
-      install_web3j
-      source_web3j
-      completed
-    fi
+tag_name=$(curl --silent "https://api.github.com/repos/hyperledger/web3j-cli/releases/latest" | jq -r .tag_name)
+web3j_version=$(echo $tag_name | sed 's/v//')
+installed_flag=0
+installed_version=""
+
+check_if_installed() {
+  if [ -x "$(command -v web3j)" ] >/dev/null 2>&1; then
+    printf 'An Web3j installation exists on your system.\n'
+    installed_flag=1
   fi
 }
 
@@ -88,17 +93,17 @@ install_web3j() {
   if [ "$(curl --write-out "%{http_code}" --silent --output /dev/null "https://github.com/hyperledger/web3j-cli/releases/download/v${web3j_version}/web3j-cli-shadow-${web3j_version}.tar")" -eq 302 ]; then
     curl -# -L -o "$HOME/.web3j/web3j-cli-shadow-${web3j_version}.tar" "https://github.com/hyperledger/web3j-cli/releases/download/v${web3j_version}/web3j-cli-shadow-${web3j_version}.tar"
     echo "Installing Web3j..."
+    echo "https://github.com/hyperledger/web3j-cli/releases/download/v${web3j_version}/web3j-cli-shadow-${web3j_version}.tar"
     tar -xf "$HOME/.web3j/web3j-cli-shadow-${web3j_version}.tar" -C "$HOME/.web3j"
     echo "export PATH=\$PATH:$HOME/.web3j" >"$HOME/.web3j/source.sh"
     chmod +x "$HOME/.web3j/source.sh"
     echo "Removing downloaded archive..."
     rm "$HOME/.web3j/web3j-cli-shadow-${web3j_version}.tar"
   else
-    echo "Looks like there was an error while trying to download Web3j."
+    echo "Looks like there was an error while trying to download web3j"
     exit 0
   fi
 }
-
 get_user_input() {
   while echo "Would you like to update Web3j [Y/n]" && read -r user_input </dev/tty ; do
     case $user_input in
@@ -137,7 +142,47 @@ source_web3j() {
       echo "Skipped update of ${bash_rc} (source string already present)"
     fi
   fi
-  # Add similar checks for other shell configurations
+  if [ -f "$HOME/.bash_profile" ]; then
+    bash_profile="${HOME}/.bash_profile"
+    touch "${bash_profile}"
+    if ! grep -qc '.web3j/source.sh' "${bash_profile}"; then
+      echo "Adding source string to ${bash_profile}"
+      printf "${SOURCE_Web3j}\n" >>"${bash_profile}"
+    else
+      echo "Skipped update of ${bash_profile} (source string already present)"
+    fi
+  fi
+  if [ -f "$HOME/.bash_login" ]; then
+    bash_login="$HOME/.bash_login"
+    touch "${bash_login}"
+    if ! grep -qc '.web3j/source.sh' "${bash_login}"; then
+      echo "Adding source string to ${bash_login}"
+      printf "${SOURCE_Web3j}\n" >>"${bash_login}"
+    else
+      echo "Skipped update of ${bash_login} (source string already present)"
+    fi
+  fi
+  if [ -f "$HOME/.profile" ]; then
+    profile="$HOME/.profile"
+    touch "${profile}"
+    if ! grep -qc '.web3j/source.sh' "${profile}"; then
+      echo "Adding source string to ${profile}"
+      printf "$SOURCE_Web3j\n" >>"${profile}"
+    else
+      echo "Skipped update of ${profile} (source string already present)"
+    fi
+  fi
+
+  if [ -f "$(command -v zsh 2>/dev/null)" ]; then
+    file="$HOME/.zshrc"
+    touch "${file}"
+    if ! grep -qc '.web3j/source.sh' "${file}"; then
+      echo "Adding source string to ${file}"
+      printf "$SOURCE_Web3j\n" >>"${file}"
+    else
+      echo "Skipped update of ${file} (source string already present)"
+    fi
+  fi
 }
 
 check_if_web3j_homebrew() {
@@ -163,6 +208,9 @@ completed() {
   echo "Web3j was successfully installed."
   echo "To use web3j in your current shell run:"
   echo "source \$HOME/.web3j/source.sh"
+  echo "When you open a new shell this will be performed automatically."
+  echo "To see what Web3j's CLI can do you can check the documentation bellow."
+  echo "https://docs.web3j.io/latest/command_line_tools/"
   printf "$RESET"
   exit 0
 }
@@ -170,14 +218,32 @@ completed() {
 check_java_version() {
   java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
   echo "Your current java version is ${java_version}"
+  # Split the Java version string into parts based on the '.' delimiter
   major_version=$(echo "$java_version" | cut -d'.' -f1)
   if [ "$major_version" -ge 17 ]; then
     echo "Your Java version is compatible with Web3j CLI."
   else
-    echo "The Web3j CLI requires a Java version equals with 17 or higher."
+    echo "The Web3j CLI requires a Java version equals with 17 or higher. Please ensure you have a compatible Java version before installing Web3j for full functionality."
     read -r -s -n 1 -p "Press any key to continue, or press Ctrl+C to cancel the installation." </dev/tty
   fi
 }
 
-# Execute main
+main() {
+  setup_color
+  check_java_version
+  check_if_installed
+  if [ $installed_flag -eq 1 ]; then
+    check_if_web3j_homebrew
+    check_version
+    clean_up
+    install_web3j
+    source_web3j
+    completed
+  else
+    install_web3j
+    source_web3j
+    completed
+  fi
+}
+
 main
